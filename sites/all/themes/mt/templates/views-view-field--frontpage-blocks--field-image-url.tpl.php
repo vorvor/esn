0ac3cac02fb675a37b0e03992b9f944d3eb22902
Borisz <borisz.voros@cheppers.com>
2017-12-09 12:03:55 +0100

    random image added if no image

--- sites/all/themes/mt/templates/views-view-field--frontpage-blocks--field-image-url.tpl.php
+++ sites/all/themes/mt/templates/views-view-field--frontpage-blocks--field-image-url.tpl.php
@@ -26,11 +26,27 @@
 
   	//dpm($row);
 
+  global $used;
+
+  if (empty($output)) {
+    $output = 'http://www.allfordmustangs.com/photopost/data/3817/0.jpg';
+    $images = file_scan_directory('public://dummy_images', '/.*/');
+    $urls = array_keys($images);
+    $output = $urls[rand(0, count($images) - 1)];
+    while (in_array($output, $used)) {
+      $output = $urls[rand(0, count($images) - 1)];
+    }
+    $used[] = $output;
+   
+    $output = str_replace('public://', '/sites/default/files/', $output);
+    
+  }
+
   if (strpos($output, 'mp3') > 0) {
     print '<img src="http://static.adweek.com/adweek.com-prod/wp-content/uploads/files/2016_Jul/iab-podcast-event-hed-2016.png"><br /><audio controls><source src="' . $output . '" type="audio/mpeg">Your browser does not support the audio element.</audio>'; 
   }
 
-  if (strpos($output, 'jpg') > 0 || strpos($output, 'png') > 0 || strpos($output, 'aspx') > 0) {
+  if (strpos($output, 'jpeg') > 0 || strpos($output, 'jpg') > 0 || strpos($output, 'png') > 0 || strpos($output, 'aspx') > 0) {
     print '<a href="/post/' . $row->nid . '"><img src="' . $output . '"></a>';
   }
 
